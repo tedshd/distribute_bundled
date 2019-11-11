e6c4a034992bc022804f4a0af85e22c42bb30182
Ted Shu <ted.shu@ileopard.com>
2019-11-11 17:22:05 +0800

    [dev] Update CSS bundle.

--- README.md
+++ README.md
@@ -7,21 +7,29 @@ Auto distribute bundled js for used page, not bundle together
 
 * webpack
 
-## gulp
-
 ```
 npm i --save-dev gulp
 npm i --save-dev gulp-watch
+npm i --save-dev gulp-cssnano
+npm i --save-dev gulp-cssnext
+npm i --save-dev gulp-postcss
+npm i --save-dev gulp-rename
+npm i --save-dev gulp-uglify
+npm i --save-dev postcss-apply
+npm i --save-dev postcss-css-variables
+npm i --save-dev autoprefixer
+npm i --save-dev webpack
 npm i --save-dev webpack-stream
+npm i --save-dev gulp-cssimport
 ```
+
+## gulp
+
+suggest version 4 up
 
 ## Webpack
 
 suggest version 4 up
-
-```
-npm i --save-dev webpack
-```
 
 ## Structure
 
@@ -32,6 +40,8 @@ developr is `src`
 You can set up in `gulpfile.js`
 
 `_common` & `_lib` put common function or library for import
+
+css files are same as js files.
 
 ## Flow
 
@@ -45,6 +55,6 @@ src/js/page2.js
 output
 
 ```
-dist/page-1573200585178.js
-dist/page2-1573200585178.js
+dist/page_1573200585178.js
+dist/page2_1573200585178.js
 ```
--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,68 @@
 const gulp = require('gulp'),
   watch = require('gulp-watch'),
+  cssnano = require('gulp-cssnano'),
+  cssimport = require("gulp-cssimport"),
+  rename = require("gulp-rename"),
+  autoprefixer = require('autoprefixer'),
+  postcssApply = require('postcss-apply'),
+  postcssVar = require('postcss-css-variables'),
+  postcss = require('gulp-postcss'),
   webpack = require('webpack-stream'),
   fs = require("fs"),
   execSync = require('child_process').execSync,
+  processors = [
+    autoprefixer({ browsers: ['last 2 version'] }),
+    postcssApply,
+    postcssVar
+  ],
   common = '_common',
   lib = '_lib',
+  cssPath = './src/css/**/',
+  cssDir = './src/css/',
   jsPath = './src/js/**/',
   jsDir = './src/js/',
   build = './dist/';
 
-gulp.task('default', function (cb) {
+gulp.task('default', function(cb) {
   var options = {};
-  watch(jsPath + '*.js', options, function (e) {
+  // watch css
+  watch(cssPath + '*.css', options, function(e) {
+    // console.log('e:'+JSON.stringify(e));
+    // console.log(new Date() + ' -- ' + e.history[0].replace(e.base, ''));
+    var path = e.history[0].replace(e.cwd, '');
+    console.log('\n');
+    console.log(new Date() + ' -- ' + path);
+
+    var ts = new Date().getTime();
+
+    (async () => {
+      var files2Build = await buildFiles(path, cssDir, '.css');
+
+      console.log(files2Build);
+
+      for (let i = 0; i < files2Build.length; i++) {
+        var name = files2Build[i].replace(cssDir, '').replace('.css', '') + '_';
+
+        // delete old file
+        execSync('find . -name "' + name + '*" -delete');
+      }
+      gulp.src(files2Build)
+      //   .pipe(concat(ts + css, { newLine: '\n' }))
+        .pipe(cssimport())
+        .pipe(postcss(processors))
+        .pipe(cssnano())
+        .pipe(rename(function (path) {
+          path.basename = path.basename + '_' + ts;
+        }))
+        .pipe(gulp.dest(build));
+    })();
+
+  });
+  // watch js
+  watch(jsPath + '*.js', options, function(e) {
     // console.log('e:'+JSON.stringify(e));
     // var filePath = e.history[0].replace(e.base, '');
     var path = e.history[0].replace(e.cwd, '');
-    var files2Build = [];
     console.log('\n');
     console.log(new Date() + ' -- ' + path);
 
@@ -23,39 +70,42 @@ gulp.task('default', function (cb) {
 
     (async () => {
 
-      if (path.search(common) !== -1 ||
-        path.search(lib) !== -1
-      ) {
-        function checkBuildingFiles () {
-          return new Promise((resolve, rejection) => {
+      // if (path.search(common) !== -1 ||
+      //   path.search(lib) !== -1
+      // ) {
+      //   function checkBuildingFiles() {
+      //     return new Promise((resolve, rejection) => {
 
-            fs.readdir(jsDir, (err, files) => {
-              let tmp = [];
-              files.forEach((file) => {
-                if (file.search('.js') !== -1) {
-                  file = jsDir + file;
-                  tmp.push(file);
-                }
-              });
-              resolve(tmp);
-            });
+      //       fs.readdir(jsDir, (err, files) => {
+      //         let tmp = [];
+      //         files.forEach((file) => {
+      //           if (file.search('.js') !== -1) {
+      //             file = jsDir + file;
+      //             tmp.push(file);
+      //           }
+      //         });
+      //         resolve(tmp);
+      //       });
 
-          });
-        }
-        files2Build = await checkBuildingFiles();
-      } else {
-        files2Build.push('.' + path);
-      }
+      //     });
+      //   }
+      //   files2Build = await checkBuildingFiles();
+      // } else {
+      //   files2Build.push('.' + path);
+      // }
+
+      // buildFiles(path, '.js').then((res) => {
+      //   console.log(res);
+      //   return res;
+      // });
+
+      var files2Build = await buildFiles(path, jsDir, '.js');
 
       console.log(files2Build);
 
-      // gulp.src(files2Build)
-      // .pipe(webpack())
-      // .pipe(gulp.dest('dist/'));
-
       var entryObj = {};
       for (let i = 0; i < files2Build.length; i++) {
-        var name = files2Build[i].replace(jsDir, '').replace('.js', '') + '-';
+        var name = files2Build[i].replace(jsDir, '').replace('.js', '') + '_';
         entryObj[name + ts] = files2Build[i];
 
         // delete old file
@@ -76,3 +126,32 @@ gulp.task('default', function (cb) {
 
   });
 });
+
+function checkBuildingFiles(dir, fileType) {
+  return new Promise((resolve, rejection) => {
+
+    fs.readdir(dir, (err, files) => {
+      let tmp = [];
+      files.forEach((file) => {
+        if (file.search(fileType) !== -1) {
+          file = dir + file;
+          tmp.push(file);
+        }
+      });
+      resolve(tmp);
+    });
+
+  });
+}
+
+async function buildFiles(path, dir, fileType) {
+  var arr = [];
+  if (path.search(common) !== -1 ||
+    path.search(lib) !== -1
+  ) {
+    arr = await checkBuildingFiles(dir, fileType);
+  } else {
+    arr.push('.' + path);
+  }
+  return arr;
+}
